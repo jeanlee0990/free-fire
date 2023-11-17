a8616999d8d37ea89c197bd1360d48b76ea36d58
ALTERIO <146441318+jeanlee0990@users.noreply.github.com>
2023-11-17 07:23:23 +0700

    Add files via upload

---
+++ kontak.js
@@ -0,0 +1,18 @@
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const url = e.target.action;
+    const formData = new FormData(contactForm);
+
+    fetch(url,{
+        method: "POST",
+        body: formData,
+        mode: "no-cors",
+    }).then(() => {
+        //url thank you
+        window.location.href = "thanks.html";
+    })
+    .catch((e) => alert("Error occured"));
+});
